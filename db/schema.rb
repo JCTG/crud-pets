e87d8d401adf0bd26b78e3225ef258bbd8fc2d25
JCTG <juan.tovar@vun.mxx>
2016-10-27 18:45:13 -0500

    Archivos faltantes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120203160956) do
+ActiveRecord::Schema.define(:version => 20161025225203) do
+
+  create_table "pets", :force => true do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
